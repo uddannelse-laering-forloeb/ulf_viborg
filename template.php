0577846ffe5298a7a93772891845ef9655a651dd
martinyde <yde001@gmail.com>
2016-02-12 13:45:26 +0100

    Changed default hamburger icon

--- template.php
+++ template.php
@@ -1,1 +1,12 @@
 <?php
+
+/**
+ * Implements hook_preprocess_page().
+ *
+ * @param $variables
+ *   Available variables.
+ */
+function ulf_aarhus_preprocess_page(&$variables) {
+  // Hamburger icon.
+  $variables['hamburger_icon_path'] = 'profiles/ulf/themes/ulf_aarhus';
+}
