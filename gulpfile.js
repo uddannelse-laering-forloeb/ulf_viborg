afece64666a7bddb1b5cef3fce77dd57a441a32b
Martin Yde Granath <my@aarhus.dk>
2019-01-07 15:28:38 +0100

    ULF-2139: Added multisite support

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,6 @@ var ngAnnotate = require('gulp-ng-annotate');
 var rename = require('gulp-rename');
 var compass = require('gulp-compass');
 
-/**
- * Setting up browsersync.
- * Proxy is the name of the vagrent.
- * Host is the the ip defined in "vagrantfile"
- */
-
-var browserSync = require('browser-sync').create();
-browserSync.init({
-  proxy: "ulf.vm",
-  host: "ulf.vm"
-});
-
-
 // We only want to process our own non-processed JavaScript files.
 var jsPath = ['./scripts/*.js', '!./js/*.min.*'];
 var sassPath = './sass/**/*.scss';
@@ -33,27 +20,40 @@ var buildDir = './js';
  * Run Javascript through JSHint.
  */
 
-gulp.task('jshint', function() {
-  return gulp.src(jsPath)
+gulp.task('jshint', function(done) {
+  gulp.src(jsPath)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+  done();
+});
+
+/**
+ * Run Javascript through JSHint.
+ */
+
+gulp.task('uglify', function(done) {
+  gulp.src(jsPath)
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '-min'
+    }))
+    .pipe(gulp.dest('./scripts/min'))
+  done();
 });
 
 /**
  * Process SCSS using libsass
  */
-gulp.task('sass', function () {
+gulp.task('sass', function (done) {
   gulp.src(sassPath)
-    .pipe(sourcemaps.init())
     .pipe(sass({
-      // outputStyle: 'compressed',
+      outputStyle: 'compressed',
       includePaths: [
-        '/usr/lib/node_modules/compass-mixins/lib'
+        './node_modules/compass-mixins/lib'
       ]
     }).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('./css'));
+  done();
 });
 
 /**
@@ -61,48 +61,46 @@ gulp.task('sass', function () {
  */
 
 gulp.task('watch', function() {
-  gulp.watch(jsPath, ['jshint']);
-  gulp.watch(sassPath, ['sass']);
-  gulp.watch(phpPath).on('change', browserSync.reload);
-  gulp.watch(jsPath).on('change',browserSync.reload);
+  gulp.watch(jsPath, gulp.series('jshint', 'uglify'));
+  gulp.watch(sassPath, gulp.series('sass'));
 });
-
 
 /**
  * Watch javascript files for changes.
  */
 
 gulp.task('js-watch', function() {
-  gulp.watch(jsPath, ['jshint']);
+  gulp.watch(jsPath, gulp.series('jshint'));
 });
 
 /**
  * Build single app.js file.
  */
-gulp.task('buildJs', function () {
+gulp.task('buildJs', function (done) {
   gulp.src(jsPath)
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write('/maps'))
     .pipe(rename({extname: ".min.js"}))
     .pipe(gulp.dest(buildDir))
+  done();
 });
 
 /**
  * Build single app.js file.
  */
-gulp.task('assetsJs', function () {
+gulp.task('assetsJs', function (done) {
   gulp.src(jsAssets)
     .pipe(concat('assets.js'))
     .pipe(rename({extname: ".min.js"}))
     .pipe(gulp.dest(buildDir))
+  done();
 });
-
 
 /**
  * Use compass
  */
-gulp.task('compass', function() {
+gulp.task('compass', function(done) {
   gulp.src(sassPath)
     .pipe(compass({
       css: 'css',
@@ -111,9 +109,9 @@ gulp.task('compass', function() {
     }))
     .pipe(minifycss())
     .pipe(gulp.dest('html/css'));
+  done();
 });
 
-
 // Tasks to compile sass and watch js file.
-gulp.task('default', ['sass', 'watch']);
-gulp.task('build', ['buildJs', 'sass']);
+gulp.task('default', gulp.parallel('sass', 'watch', 'uglify'));
+gulp.task('build', gulp.parallel('buildJs', 'sass', 'uglify'));
